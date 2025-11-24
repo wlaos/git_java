bbbb2e931e6e480ac9e5f2ee3aff32ae9e4e8093
wlaos <dkdlfjsl0719@gmail.com>
2025-11-24 14:09:50 +0900

    branch_test에서 수정

--- git_java/src/git_java/Main.java
+++ git_java/src/git_java/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test1");
-		System.out.println("branch_test1");
-		System.out.println("test2 ");
+		System.out.println("test123");
+		System.out.println("branch_test에서 수정");
 	}
 
 }
